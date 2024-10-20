4bd4016d89f6cdca6a2fa92b93254c90a2cd8b18
CarlosPaz64 <134614533+CarlosPaz64@users.noreply.github.com>
2024-10-19 20:10:48 -0600

    Cambios

--- src/services/productosServices.ts
+++ src/services/productosServices.ts
@@ -1,13 +1,32 @@
-import { Producto, ProductoModel } from '../models/productoModel';
+import { ProductoModel, NuevoProducto, Producto } from '../models/productoModel'; 
 
 export class ProductoService {
-  // Obtener todos los productos
+  // Servicio para obtener todos los productos
   static async obtenerProductos(): Promise<Producto[]> {
-    return await ProductoModel.obtenerTodos();
+    try {
+      const productos = await ProductoModel.obtenerTodos();
+      return productos;
+    } catch (error) {
+      throw new Error('Error al obtener productos');
+    }
   }
 
-  // Crear un nuevo producto
-  static async crearProducto(producto: Producto): Promise<void> {
-    await ProductoModel.crearProducto(producto);
+  // Servicio para crear un nuevo producto (sin product_id)
+  static async crearProducto(nuevoProducto: NuevoProducto): Promise<void> {
+    try {
+      await ProductoModel.crearProducto(nuevoProducto);
+    } catch (error) {
+      throw new Error('Error al crear producto');
+    }
+  }
+
+  // Servicio para obtener productos por categoría
+  static async obtenerProductosPorCategoria(): Promise<Producto[]> {
+    try {
+      const productos = await ProductoModel.obtenerTodosOrdenadosPorCategoria();
+      return productos;
+    } catch (error) {
+      throw new Error('Error al obtener productos por categoría');
+    }
   }
 }
